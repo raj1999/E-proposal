813639342692780e776f0bbb1c9001a21c75da9f
raj1999 <41324284+raj1999@users.noreply.github.com>
2020-09-29 13:57:50 +0530

    Add files via upload
    CSS, js, and images added

---
+++ js/query-file.js
@@ -0,0 +1,9 @@
+$('.card-text').hover(function(){
+    $(this).toggleClass('high');
+});
+$('.card-body').css('list-style','none');
+// $('a').css('text-decoration','none');
+$('a').css('color', "#a09d9d");
+$("li").hover( function(){
+ $(this).toggleClass("high");
+});
